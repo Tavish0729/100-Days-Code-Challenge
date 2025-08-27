ec32983dc759da7ccf47aeda2f5cf4eb5d0fbebd
Tavish0729 <tavishmahajan1234@gmail.com>
2025-08-27 12:53:02 +0530

    day7ques2.c

--- day7ques2.c
+++ day7ques2.c
@@ -3,24 +3,6 @@
 int main() {
     char ch;
 
-    // Input a character
+  
     printf("Enter an alphabet: ");
-    scanf("%c", &ch);
-
-    // Check if the character is an alphabet
-    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-        
-        // Check for vowels
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-            printf("%c is a Vowel.\n", ch);
-        } else {
-            printf("%c is a Consonant.\n", ch);
-        }
-    } 
-    else {
-        printf("Invalid input! Please enter an alphabet.\n");
-    }
-
-    return 0;
-}
+    
