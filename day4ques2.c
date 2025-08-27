37044a77b483962ed5aa0726ef06c114a2b2e2f3
Tavish0729 <tavishmahajan1234@gmail.com>
2025-08-27 12:49:18 +0530

    day4ques2.c

--- day4ques2.c
+++ day4ques2.c
@@ -3,17 +3,9 @@
 int main() {
     int n, i, sum = 0;
 
-    // Input value of n
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    // Calculate sum using loop
-    for (i = 1; i <= n; i++) {
-        sum = sum + i;
-    }
-
-    // Display result
-    printf("The sum of the first %d natural numbers is: %d\n", n, sum);
-
     return 0;
 }
+
