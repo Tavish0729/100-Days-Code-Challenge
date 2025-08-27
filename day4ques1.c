6c69c2241750a21d39c40f1bdc481113011d5f48
Tavish0729 <tavishmahajan1234@gmail.com>
2025-08-27 12:48:41 +0530

    day4ques1.c

--- day4ques1.c
+++ day4ques1.c
@@ -3,22 +3,20 @@
 int main() {
     int a, b;
 
-    // Input two numbers
     printf("Enter first number: ");
     scanf("%d", &a);
     printf("Enter second number: ");
     scanf("%d", &b);
 
-    // Display before swapping
     printf("\nBefore swapping: a = %d, b = %d\n", a, b);
 
-    // Swapping without third variable (using addition and subtraction)
-    a = a + b;  // sum stored in a
-    b = a - b;  // original a stored in b
-    a = a - b;  // original b stored in a
 
-    // Display after swapping
+    a = a + b;  
+    b = a - b; 
+    a = a - b;  
+
     printf("After swapping: a = %d, b = %d\n", a, b);
 
     return 0;
 }
+
