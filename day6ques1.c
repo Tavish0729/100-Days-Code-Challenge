0f49d8e1e403bd59491b96f00b5c988fe95bb455
Tavish0729 <tavishmahajan1234@gmail.com>
2025-08-27 12:51:12 +0530

    day6ques1.c

--- day6ques1.c
+++ day6ques1.c
@@ -3,16 +3,15 @@
 int main() {
     int num;
 
-    // Input a number
     printf("Enter an integer: ");
     scanf("%d", &num);
 
-    // Check even or odd
-    if (num % 2 == 0) {
+    if (num % 2 == 0) 
         printf("%d is Even.\n", num);
-    } else {
+     else 
         printf("%d is Odd.\n", num);
-    }
+    
 
     return 0;
 }
+
