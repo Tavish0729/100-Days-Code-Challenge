a5dad606ef4a150e1a8501577c5a23d41e7026d5
Tavish0729 <tavishmahajan1234@gmail.com>
2025-08-27 12:47:08 +0530

    day3ques1.c

--- day3ques1.c
+++ day3ques1.c
@@ -3,15 +3,13 @@
 int main() {
     float celsius, fahrenheit;
 
-    // Input temperature in Celsius
     printf("Enter temperature in Celsius: ");
     scanf("%f", &celsius);
 
-    // Convert Celsius to Fahrenheit
     fahrenheit = (celsius * 9 / 5) + 32;
 
-    // Display result
     printf("Temperature in Fahrenheit = %.2f\n", fahrenheit);
 
-    return 0; // important
+    return 0; 
 }
+
