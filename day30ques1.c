6e456e2de6894b325723d09feb23ab90d83a19ad
Tavish0729 <tavishmahajan1234@gmail.com>
2025-11-02 21:17:13 +0530

    Add files via upload

---
+++ day30ques1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, even = 0, odd = 0;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Count of even numbers = %d\n", even);
+    printf("Count of odd numbers = %d\n", odd);
+
+    return 0;
+}
---
+++ day30ques2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, pos = 0, neg = 0, zero = 0;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] > 0)
+            pos++;
+        else if (arr[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+
+    printf("Positive numbers = %d\n", pos);
+    printf("Negative numbers = %d\n", neg);
+    printf("Zeros = %d\n", zero);
+
+    return 0;
+}
