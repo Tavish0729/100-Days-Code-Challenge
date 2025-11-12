7aed63b1fb6747edbdec4131f1faf07b0993e300
Tavish0729 <tavishmahajan1234@gmail.com>
2025-11-12 20:53:43 +0530

    day61

---
+++ day61ques1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i, j;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter window size k: ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid window size.\n");
+        return 0;
+    }
+
+    printf("First negative integers in each window: ");
+    for (i = 0; i <= n - k; i++) {
+        int found = 0;
+        for (j = i; j < i + k; j++) {
+            if (arr[j] < 0) {
+                printf("%d ", arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+            printf("0 ");
+    }
+    printf("\n");
+    return 0;
+}
