daecf2e78971c3d713b2602fe00ed34c65078bb6
Tavish0729 <tavishmahajan1234@gmail.com>
2025-11-22 21:25:01 +0530

    day84

---
+++ day84ques1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum Status { SUCCESS, FAILURE, TIMEOUT };
+
+int main() {
+    enum Status s;
+    scanf("%d", &s);
+
+    if (s == SUCCESS) printf("Operation Successful");
+    else if (s == FAILURE) printf("Operation Failed");
+    else if (s == TIMEOUT) printf("Operation Timed Out");
+    else printf("Invalid");
+
+    return 0;
+}
