d23ee002d0675874eaa9d5872edc9d3a6c94b178
Tavish0729 <tavishmahajan1234@gmail.com>
2025-11-23 21:13:07 +0530

    day86

---
+++ day86ques1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+enum Menu { ADD = 1, SUBTRACT, MULTIPLY };
+
+int main() {
+    enum Menu choice;
+    int a, b;
+
+    scanf("%d", &choice);
+    scanf("%d %d", &a, &b);
+
+    switch (choice) {
+        case ADD: printf("%d", a + b); break;
+        case SUBTRACT: printf("%d", a - b); break;
+        case MULTIPLY: printf("%d", a * b); break;
+        default: printf("Invalid");
+    }
+
+    return 0;
+}
